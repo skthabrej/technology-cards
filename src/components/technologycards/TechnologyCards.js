a245cc7d4d8eafd4bcaca37aa4afcdf69de6f462
Shaik Thabrej Basha <skthabrej@gmail.com>
2022-10-08 17:34:13 +0530

    TechnologyCards

---
+++ src/components/technologycards/TechnologyCards.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Component } from "react";
+import './TechnologyCards.css'
+
+class TechnologyCards extends Component {
+    render() {
+        return(
+            <div>
+                <h1>vv</h1>
+            </div>
+        )
+    }
+}
+
+export default TechnologyCards
